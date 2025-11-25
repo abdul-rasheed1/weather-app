0d9482b364535c6524a9c5b97b35658f220d6d9b
rasheed <rasheedjawula18@gmail.com>
2025-11-25 02:37:09 +0000

    refactored to include weather icon

--- src/components/weatherData/weatherDisplay.jsx
+++ src/components/weatherData/weatherDisplay.jsx
@@ -1,9 +1,34 @@
-function WeatherDisplay ({weatherData}){
+const kelvinToCelsius = (K) => K - 273.15;
+
+const KelvinToFahrenheit = (K) =>{
+	const C = K - 273.15;
+	const Fahr = C * 9/5 + 32;
+	return Fahr;
+
+}
+
+function WeatherDisplay ({weatherData, unit, unitToggle}){
+
+	const Ktemp = weatherData.main.temp;
+
+	const iconCode = weatherData.weather[0].icon;
+
+	const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`
+
+	const DisplayTemp = unit === 'C'? kelvinToCelsius(Ktemp) : KelvinToFahrenheit(Ktemp);
+
+	const RoundTemp = Math.round(DisplayTemp);
+
 	return(
 		<div className="weather-Display">
 			<h2>{weatherData.name}</h2>
 
-			<h4>Temperature: {weatherData.main.temp}</h4>
+			<img src={iconUrl} className="weather-icon" alt="weather icon"/>
+
+			<h4>Temperature: {RoundTemp}˚{unit}</h4>
+			<button onClick={unitToggle}>
+				Switch to {unit === 'C'? 'F' : 'C'}
+			</button>
 			
 		</div>
 		)
